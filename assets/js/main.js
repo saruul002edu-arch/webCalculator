724676812f3ac6be4cb9a97f6a81c77678de1149
saruul002edu-arch <saruul002.edu@gmail.com>
2026-06-08 23:43:16 +0800

    calculator project

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+const display = document.getElementById('display');
+
+function appendValue(value) {
+    if (value === '.') {
+        if (display.value.includes('.')){
+            return;
+        }
+    }
+
+    if(display.value === '0' && value !== '.'){
+        display.value = value;
+    } else {
+        display.value +=value;
+    }
+}
+
+function clearDisplay(){
+    display.value = '0';
+}
+
+function calculate(){
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'ERROR!';
+    }
+}
